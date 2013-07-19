c1d53d06761866e0c8b294e22872b7b257c5c125
Michelle Fullwood <michelle.fullwood@gmail.com>
2013-07-19 18:05:13 -0400

    Added slide and code about nesting parentheses

---
+++ code_for_slides/find_all_nested.py
@@ -0,0 +1,10 @@
+import re
+
+name_regex = r'\b(([A-Z][a-z]+)\b (\b[A-Z][a-z]+))\b'
+print re.findall(name_regex, 
+    '"Oliver Twist" was written by Charles Dickens')
+    
+# returns [('Oliver Twist', 'Oliver', 'Twist'), 
+#          ('Charles Dickens', 'Charles', 'Dickens')]
+
+
