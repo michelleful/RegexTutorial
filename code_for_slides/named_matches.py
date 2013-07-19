0c22aeb653a66ff1a1c995a6839dfc02be2c1675
Michelle Fullwood <michelle.fullwood@gmail.com>
2013-07-19 18:39:26 -0400

    Reformatted example from Python docs

--- code_for_slides/named_matches.py
+++ code_for_slides/named_matches.py
@@ -1,8 +1,6 @@
 # code from the Python docs
->>> m = re.match(r"(?P<first_name>\w+) 
-                   (?P<last_name>\w+)", 
-                 "Malcolm Reynolds")
->>> m.group('first_name')
-'Malcolm'
->>> m.group('last_name')
-'Reynolds'
+m = re.match(r"(?P<first_name>\w+) 
+               (?P<last_name>\w+)", 
+               "Malcolm Reynolds")
+m.group('first_name') # 'Malcolm'
+m.group('last_name')  # 'Reynolds'
