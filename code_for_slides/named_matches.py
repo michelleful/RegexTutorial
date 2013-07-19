9ce326d9897789d752168205d4a6233f0068bbc2
Michelle Fullwood <michelle.fullwood@gmail.com>
2013-07-19 14:26:05 -0400

    Slide on named matches

---
+++ code_for_slides/named_matches.py
@@ -0,0 +1,8 @@
+# code from the Python docs
+>>> m = re.match(r"(?P<first_name>\w+) 
+                   (?P<last_name>\w+)", 
+                 "Malcolm Reynolds")
+>>> m.group('first_name')
+'Malcolm'
+>>> m.group('last_name')
+'Reynolds'
