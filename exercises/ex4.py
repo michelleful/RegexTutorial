095bdb6a106a02981c17180056134bc1ded13b56
Michelle Fullwood <michelle.fullwood@gmail.com>
2013-07-30 17:24:33 -0400

    Further edited ex4

--- exercises/ex4.py
+++ exercises/ex4.py
@@ -3,9 +3,10 @@
 import re
 
 # Instructions: first, run this program from the command line
-# You'll see that it fails the first test. Fix the regex
-# so that it passes the first test. Does it then pass the second
-# test? If not, fix it again!
+# You'll see that it fails the first test. Why? (Discuss.)
+
+# Fix the regex so that it passes the first test. 
+# Does it then pass the second test? If not, fix it again!
 
 price_regex = r'$[0-5].\d\d' # match strings $0.00 to $5.99
 
