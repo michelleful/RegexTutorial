9a4de7e711afa5ad32387daf1a0a3606589b7133
Michelle Fullwood <michelle.fullwood@gmail.com>
2013-07-30 17:12:13 -0400

    Edited exercise 4 to be more intuitive

--- exercises/ex4.py
+++ exercises/ex4.py
@@ -1,34 +1,17 @@
-# Exercise 4a - match prices $5.99 and less
-
+# Exercise 4 - match prices $5.99 and less
+# -----------------------------------------
 import re
 
-price_regex = r'$[0-5].\d\d' # match strings $0.00 to $5.99
-
-if not re.match(price_regex, '$3.99'):
-   print "Uh oh, it didn't match!"
-
-# Why doesn't this work?
-
-# Exercise 4b - fixing price regex
-
-# Fix the price_regex below and try running this code block
+# Instructions: first, run this program from the command line
+# You'll see that it fails the first test. Fix the regex
+# so that it passes the first test. Does it then pass the second
+# test? If not, fix it again!
 
 price_regex = r'$[0-5].\d\d' # match strings $0.00 to $5.99
 
 if not re.match(price_regex, '$3.99'):
-    print "Uh oh, it didn't match!"
-    
-if re.match(price_regex, '$3999'):
+    print "Uh oh, it didn't match $3.99!"
+elif re.match(price_regex, '$3999'):
     print "Uh oh, it matched $3999!"
-
-# Exercise 4c - fixing price regex
-
-# Fix the price_regex below and try running the code block (note: you'll have to fix two things!)
-
-price_regex = r'$[0-5].\d\d' # match strings $0.00 to $5.99
-
-if not re.match(price_regex, '$3.99'):
-    print "Uh oh, it didn't match!"
-    
-if re.match(price_regex, '$3999'):
-    print "Uh oh, it matched $3999!"
+else:
+    print "Congratulations, you fixed it!"
