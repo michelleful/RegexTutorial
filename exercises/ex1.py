a125e93d4c2b8c1823507cf75a19edfbd90c5363
Michelle Fullwood <michelle.fullwood@gmail.com>
2013-08-14 16:26:00 -0400

    Edited ex1 for clarity

--- exercises/ex1.py
+++ exercises/ex1.py
@@ -2,17 +2,19 @@
 
 import re
 
-# Match sequences that look like this: A1B C2D - pairs of letter-digit-letter sequences, separated by a space-lik character.
+# Match sequences that look like this: A1b c2D - pairs of letter-digit-letter sequences, separated by a space-lik character.
 # Your regular expression should consist of a string of metacharacters.
 
 regex  = r'...' # replace the ellipsis with a suitable regular expression
 
-# What happens if we try to match a matching string?
+# Then run the script and look at the output of the print statements below.
 
-string = 'A1b c2D' # you can change this around a little
-print re.match(regex, string) 
+# What happens if we try to match a matching string?
+string = 'A1b c2D'
+print re.match(regex, string)
 
 # What happens if we try to match a non-matching string?
-
-string = 'A1B_C2D' # you can change this around a little!
+string = 'A1b_C2D'
 print re.match(regex, string)
+
+
