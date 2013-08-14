f50ba5a855f20b786c8d1961cc958ea0e64f9e0e
Michelle Fullwood <michelle.fullwood@gmail.com>
2013-08-14 16:34:41 -0400

    Edited name matching slide and back ref slide

--- code_for_slides/name_match.py
+++ code_for_slides/name_match.py
@@ -1,6 +1,7 @@
 import re
 
 name_regex = r'\b([A-Z][a-z]+)\b \b([A-Z][a-z]+)\b'
+# Note:this is a TERRIBLE way to check for names. Why?
 m = re.search(name_regex, '"Oliver Twist" was written by Charles Dickens')
 if m:
     print m.groups()
